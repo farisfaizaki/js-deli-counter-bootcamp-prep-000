0f5043659282679e1fd4b36c3b5feb58f42cbf6c
Akhmad Faris Faizaki <faris.faizaki@gmail.com>
2017-08-29 10:16:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ function takeANumber(line,name){
   var string = `Welcome, ${name}. You are number ${number} in line.`;
   return string;
 }
+
+function nowServing(line){
+  if(line.length>0){
+    var string = `Currently serving ${line[0]}.`
+    line.shift();
+  }else{
+    var string = `There is nobody waiting to be served!`;
+  }
+  return string;  
+  
+}
