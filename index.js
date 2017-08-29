51c01a104016530b0123a5a0054a51c6a4332ac1
Akhmad Faris Faizaki <faris.faizaki@gmail.com>
2017-08-29 10:12:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeliLine = [];
 
 function takeANumber(line,name){
   line.push(name);
-  var number = line.length+1;
+  var number = line.length;
   var string = `Welcome, ${name}, You are number ${number} in line`;
   return string;
 }
